f9e5d62dd6b4c963f65cf7a25384c00b2042a095
Goroshchenko <mixael@yandex.ru>
2023-01-10 20:25:15 +0300

    1 point

--- personGenerator.js
+++ personGenerator.js
@@ -36,10 +36,26 @@ const personGenerator = {
         }
     }`,
 
+    firstNameFemaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Александра",
+            "id_2": "Евгения",
+            "id_3": "Екатерина",
+            "id_4": "Антонина",
+            "id_5": "Ольга",
+            "id_6": "Прасковья",
+            "id_7": "Валентина",
+            "id_8": "Дарья",
+            "id_9": "Мария",
+            "id_10": "Ирина"
+        }
+    }`,
+
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
 
-    randomIntNumber: (max = 1, min = 0) => Math.floor(Math.random() * (max - min + 1) + min),
+    randomIntNumber: (max = 2023, min = 1900) => Math.floor(Math.random() * (max - min + 1) + min),
 
     randomValue: function (json) {
         const obj = JSON.parse(json);
@@ -48,23 +64,31 @@ const personGenerator = {
     },
 
     randomFirstName: function() {
-
-        return this.randomValue(this.firstNameMaleJson);
+        if (this.person.gender === 'Мужчина') {
+            return this.randomValue(this.firstNameMaleJson);
+        }
+            return this.randomValue(this.firstNameFemaleJson);
 
     },
-
 
      randomSurname: function() {
-
-        return this.randomValue(this.surnameJson);
-
+         if (this.person.gender === 'Мужчина') {
+             return this.randomValue(this.surnameJson);
+         }
+            return (this.randomValue(this.surnameJson) + JSON.parse(JSON.stringify('a')));
     },
 
+    randomGender: function() {
+       const numberOfGender = Math.round( Math.random());
+       return (numberOfGender) ? this.GENDER_MALE : this.GENDER_FEMALE
+    },
 
     getPerson: function () {
         this.person = {};
-        // this.person.gender = this.randomGender();
-        this.person.firstName = this.randomFirstName();
+        this.person.gender = this.randomGender();
+        this.person.firstName = this.randomFirstName(this.person.gender);
+        this.person.surname = this.randomSurname(this.person.gender);
+        this.person.birthYear = this.randomIntNumber();
         return this.person;
     }
 };
